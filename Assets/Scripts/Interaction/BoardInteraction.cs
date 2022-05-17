a63d9cf17c89a816ab54236c227470b3efb52095
camila15 <mcamilas1599@gmail.com>
2022-05-16 19:16:45 -0500

    video ok

---
+++ Assets/Scripts/Interaction/BoardInteraction.cs
@@ -0,0 +1,16 @@
+﻿
+using VRC.SDKBase;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class BoardInteraction : MonoBehaviour
+{
+    [SerializeField] VideoPlayer video;
+    [SerializeField] BoxCollider player;
+    private void OnTriggerEnter(Collider other) {
+        if(other.CompareTag("Player")) video.Play();
+        
+    }
+}
